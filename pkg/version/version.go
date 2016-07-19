1f0b422673fc03359a18604e57f60cab4874165a
Chi Vinh Le <vinh@wikiwi.io>
2016-07-19 19:28:44 +0700

    Start new development cycle

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -10,8 +10,11 @@ package version
 
 // Version is the current version of the kube-volume-freezer.
 // Update this whenever making a new release.
-// The version is of the format Major.Minor.Patch
+// The version is of the format Major.Minor.Patch[-Stage.No[+]]
 // Increment major number for new feature additions and behavioral changes.
 // Increment minor number for bug fixes and performance enhancements.
 // Increment patch number for critical fixes to existing releases.
-var Version = "0.1.0-rc.2"
+// Stage is added during development and is one of (pre-alpha|alpha|beta|rc)
+// accompanied by its iteration number. The trailing '+' sign is only removed
+// during a release.
+var Version = "0.1.0-rc.3+"
